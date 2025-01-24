2e34b480cfa9cb066a0aca225ae693b61f416b7a
robinAZERTY <robinlecouffe@gmail.com>
2025-01-24 21:25:27 +0100

    Update animation steps to include measurement probability distribution and enhance state estimation visuals

--- manimations/step.py
+++ manimations/step.py
@@ -75,8 +75,7 @@ class Step(MovingCameraScene):
         ).shift(DOWN*0.5)
 
         graph = axes.plot(lambda x: (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2), color=BLUE)
-        label = MathTex(r"p(\hat{X}_{k-1})").next_to(graph, UP, buff=0.1).shift(RIGHT)
-        label[0][2:-1].set_color(BLUE)
+        label = MathTex(r"\hat{X}_{k-1}").next_to(graph, UP, buff=0.1).shift(RIGHT).set_color(BLUE)
         # graph_label = axes.get_graph_label(graph, label=label, x_val=mu, direction=RIGHT).shift(RIGHT*0.5+UP*0.5)
 
         mu_label = MathTex(str(mu)).next_to(axes.c2p(mu, 0), DOWN)
@@ -91,10 +90,11 @@ class Step(MovingCameraScene):
         mu = 19
         sigma = 1
         predicted = axes.plot(lambda x: (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2), color=BLUE)
-        predictedLabel = MathTex(r"p(\hat{X}_{k})").next_to(predicted, UP, buff=0.1).shift(RIGHT)
-        predictedLabel[0][2:-1].set_color(BLUE)
+        predictedLabel = MathTex(r"\hat{X}_{k}").next_to(predicted, UP, buff=0.1).set_color(BLUE)
         predictedmu_label = MathTex(str(mu)).next_to(axes.c2p(mu, 0), DOWN)
         predictedmu_dot = Dot(point=axes.c2p(mu, 0), color=BLUE)
+        
+        predGroup = VGroup(predicted, predictedLabel, predictedmu_dot, predictedmu_label)
         
         # fleche vers la droite pour illustrer la commande
         commandeArrow = Arrow(axes.c2p(18, 0.3), axes.c2p(20, 0.3), buff=0)
@@ -102,7 +102,11 @@ class Step(MovingCameraScene):
         
         self.play(Write(commandeArrow),Write(commande))
         self.wait(2)
-        self.play(FadeOut(commandeArrow),FadeOut(commande),Transform(graph,predicted), Transform(label,predictedLabel), Transform(mu_dot,predictedmu_dot), Transform(mu_label,predictedmu_label))
+        # duration of transition is 3s
+        self.play(FadeOut(commandeArrow),FadeOut(commande),Transform(graph,predicted), Transform(label,predictedLabel), Transform(mu_dot,predictedmu_dot), Transform(mu_label,predictedmu_label),run_time = 3)
+    
+        self.wait(2)
+        self.play(FadeOut(axes),FadeOut(graph),FadeOut(label),FadeOut(predictedLabel),FadeOut(mu_dot),FadeOut(mu_label),FadeOut(pred))
 
 
         Equation = MathTex(r"\hat{X}_{k} = f(\hat{X}_{k-1}, U_{k})").scale(1)
@@ -110,18 +114,20 @@ class Step(MovingCameraScene):
         Equation[0][6:11].set_color(BLUE)
         Equation[0][12:14].set_color(ORANGE)
         
-        self.play(FadeOut(predicted),FadeOut(predictedLabel),FadeOut(predictedmu_dot),FadeOut(predictedmu_label))
-        self.play(Write(Equation))
-        self.wait(2)
-        self.play(FadeOut(Equation))
+        # self.play(FadeOut(predicted),FadeOut(predictedLabel),FadeOut(predictedmu_dot),FadeOut(predictedmu_label))
+        # self.play(Write(Equation))
+        # self.wait(2)
+        # self.play(FadeOut(Equation))
         
                 
         # self.play(FadeOut(a1), FadeOut(a2), Transform(VGroup(prevState, command, predState), Equation))
         # self.wait(2)
         
         # self.play(FadeOut(Equation), FadeOut(pred),FadeOut(VGroup(prevState, command, predState)))
-        # self.wait(2)
+        self.wait(2)
 
+
+        
         
         prevState = Text("estimation précédente").scale(0.75).to_edge(LEFT).set_color(BLUE).shift(RIGHT*0.75)
         prevStateex = MathTex(r"19°C").scale(1).move_to(prevState).set_color(BLUE)
@@ -139,19 +145,45 @@ class Step(MovingCameraScene):
         
         upd.move_to(pred)
         
-        # a2 = Arrow(mesure, predState)
-        # self.play(FadeIn(upd))
-        # self.wait(2)
-        # self.play(Write(prevState), Write(mesure),Write(predState),Write(a1),Write(a2))
-        # self.wait(1)
-        # self.play(Transform(prevState, prevStateex))
-        # self.wait(1)
-        # self.play(Transform(mesure, mesureex))
-        # self.wait(1)
-        # self.play(Transform(predState, predStateex))
-        # self.wait(3)
-        # self.play(Transform(prevState, XhatMOne), Transform(mesure, U), Transform(predState, Xhat))
-        # self.wait(2)
+        a2 = Arrow(mesure, predState)
+        self.play(FadeIn(upd))
+        self.wait(2)
+        self.play(Write(prevState), Write(mesure),Write(predState),Write(a1),Write(a2))
+        self.wait(3)
+        
+        self.play(FadeOut(predState), FadeOut(a1), FadeOut(a2), FadeOut(prevState), FadeOut(mesure))
+        self.play(Write(predGroup), Write(axes))
+        
+        # Adding the probability distribution of mesure
+        mu = 22
+        sigma = 1.5
+        
+        mesurment = axes.plot(lambda x: (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2), color=RED)
+        mesurmentLabel = MathTex(r"Z_{k}").next_to(mesurment, UP, buff=0.1).shift(RIGHT).set_color(RED)
+        mu_label = MathTex(str(mu)).next_to(axes.c2p(mu, 0), DOWN)
+        mu_dot = Dot(point=axes.c2p(mu, 0), color=RED)
+        MesurmentGroup = VGroup(mesurment, mesurmentLabel, mu_dot, mu_label)
+        
+        self.play(Write(MesurmentGroup))
+        self.wait(2)
+        
+        # Updating the graph with the new mu and sigma
+        mu = 20
+        sigma = 0.8
+        
+        updated = axes.plot(lambda x: (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma)**2), color=BLUE)
+        updatedLabel = MathTex(r"\hat{X}_{k+1}").next_to(updated, UP, buff=0.1).shift(RIGHT).set_color(BLUE)
+        mu_label = MathTex(str(mu)).next_to(axes.c2p(mu, 0), DOWN)
+        mu_dot = Dot(point=axes.c2p(mu, 0), color=BLUE)
+        
+        UpdatedGroup = VGroup(updated, updatedLabel, mu_dot, mu_label)
+        
+        self.play(Transform(predGroup, UpdatedGroup), FadeOut(MesurmentGroup), run_time=3)
+        self.wait(2)
+        
+        
+        
+        
         
         
         
